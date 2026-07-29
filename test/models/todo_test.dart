39370dca8c9b709b444fe84cc3f8f7d827699c75
tea35 <blackapple3511@gmail.com>
2026-07-29 17:25:04 +0900

    feat: Flutter CIを設定し、ビルドとテストの自動化を実装

---
+++ test/models/todo_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:todo_app/models/todo.dart';
+
+void main() {
+  group('Todo', () {
+    test('初期状態ではisDoneがfalseになる', () {
+      final todo = Todo(title: 'テストタスク');
+      expect(todo.title, 'テストタスク');
+      expect(todo.isDone, false);
+    });
+
+    test('isDoneをtrueで初期化できる', () {
+      final todo = Todo(title: 'テストタスク', isDone: true);
+      expect(todo.isDone, true);
+    });
+  });
+}
